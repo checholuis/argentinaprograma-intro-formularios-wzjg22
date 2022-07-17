903076c722619997b59425a26aaf153995b11d4e
checholuis <joseluisaquino25@gmail.com>
2022-07-16 22:54:35 -0300

    Ejercicio terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { FormControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-
-export class LoginComponent implements OnInit {
-  form: FormGroup;
-  
-  constructor(private formBuilder: FormBuilder {
-    this.form = this.formBuilder.group( {
-      user:['',[]],
-      mail: ['',[]],
-      password: ['',[]]
-    })
-  })
-  
-  ngOnInit() {}
-}
-
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,54 +5,58 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
   // Inyectar en el constructor el formBuilder
-  constructor(private formBuilder: FormBuilder){ 
+  constructor(private formBuilder: FormBuilder) {
     ///Creamos el grupo de controles para el formulario de login
-    this.form= this.formBuilder.group({
-      password:['',[Validators.required, Validators.minLength(8)]],
-      email:['', [Validators.required, Validators.email]],
-   })
+    this.form = this.formBuilder.group({
+      username:['',[Validators.required, Validators.minLength(5),Validators.maxLength(12)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
   }
 
   ngOnInit() {}
 
-  get Password(){
-    return this.form.get("password");
-  }
- 
-  get Mail(){
-   return this.form.get("email");
+  get Password() {
+    return this.form.get('password');
   }
 
-  get PasswordValid(){
+  get Mail() {
+    return this.form.get('email');
+  }
+
+  get Username() {
+    return this.form.get('username');
+  }
+
+  get UsernameValid() {
+    return this.Username?.touched && !this.Username?.valid;
+  }
+
+  get PasswordValid() {
     return this.Password?.touched && !this.Password?.valid;
   }
 
   get MailValid() {
-    return false
+    return this.Mail?.touched && !this.Mail?.valid;
   }
- 
 
-  onEnviar(event: Event){
+  onEnviar(event: Event) {
     // Detenemos la propagación o ejecución del compotamiento submit de un form
-    event.preventDefault; 
- 
-    if (this.form.valid){
+    event.preventDefault;
+
+    if (this.form.valid) {
       // Llamamos a nuestro servicio para enviar los datos al servidor
       // También podríamos ejecutar alguna lógica extra
-      alert("Todo salio bien ¡Enviar formuario!")
-    }else{
-      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template     
-      this.form.markAllAsTouched(); 
+      alert('Todo salio bien ¡Enviar formuario!');
+    } else {
+      // Corremos todas las validaciones para que se ejecuten los mensajes de error en el template
+      this.form.markAllAsTouched();
     }
- 
   }
-
-
 }
